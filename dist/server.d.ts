836164d9283870ea39272cc28ae91f307bda9c42
Fabian Klingenberg <klingenberg.fabian@gmx.de>
2025-10-22 18:55:11 +0200

    little refactor

--- dist/server.d.ts
+++ dist/server.d.ts
@@ -1,7 +1,7 @@
 import { Window } from "happy-dom";
 import { JSDOM } from "jsdom";
 declare let renderer: "happy-dom" | "jsdom";
-declare function getLibrary(options: options): Promise<typeof import("hydro-js")>;
+declare function getLibrary(options?: options): Promise<typeof import("hydro-js")>;
 declare function renderRootToString(): string;
 declare function renderToString(elem: Element): string;
 declare function setRenderer(newRenderer: typeof renderer): void;
